9d066c870dc2f40bffa788bbe7d517c5bd7843e0
unknown <drmdfoureilu@gmail.com>
2018-10-22 17:31:38 -0500

    adding stuff

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from posts.models import Post 
 # Create your views here.
-def post_home(request):
+def post_create(request):
+    return HttpResponse("<h1>>:V</h1>")
+
+def post_detail(request):
+	#instance = Post.objects.get(id=1)
+	instance = get_object_or_404(Post,id=1)
+	context={
+	"titulo":instance.titulo,
+	"instance":instance,
+	}
+	return render(request,"post_detail.html",context)
+
+def post_list(request):
+	queryset = Post.objects.all()
+	context={
+	"titulo":"List",
+	"object_list":queryset,
+	}
+	return render(request,"index.html",context)
+
+def post_update(request):
+    return HttpResponse("<h1>>:V</h1>")
+
+def post_delete(request):
     return HttpResponse("<h1>>:V</h1>")
