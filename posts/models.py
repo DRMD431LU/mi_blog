50625a1fa47b1fe5f066fcb1b434ec95bef60077
unknown <drmdfoureilu@gmail.com>
2018-10-24 19:21:03 -0500

    adding form.py

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 
@@ -11,3 +12,6 @@ class Post(models.Model):
 
 	def __str__(self):
 		return self.titulo
+
+	def get_absolute_url(self):
+		return reverse("posts:detail", kwargs={"id":self.id})
