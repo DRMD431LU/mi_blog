046789a67ed41f8e091a9da959fa6200cc36e620
unknown <drmdfoureilu@gmail.com>
2018-10-29 18:54:54 -0600

    adding update and delete view

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseRedirect
 from posts.models import Post 
 from .forms import PostForm
+from django.contrib import messages
 # Create your views here.
 def post_create(request):
-	form =PostForm()
+	form =PostForm(request.POST or None)
+	#if request.method == "POST":
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		messages.success(request, "Tu post se ha creado :D")
+		return HttpResponseRedirect(instance.get_absolute_url())
 	context = {
 		"form": form
 	}
@@ -25,10 +32,25 @@ def post_list(request):
 	"titulo":"List",
 	"object_list":queryset,
 	}
-	return render(request,"index.html",context)
+	return render(request,"base.html",context)
 
-def post_update(request):
-    return HttpResponse("<h1>>:V</h1>")
+def post_update(request, id=None):
+	instance = get_object_or_404(Post, id= id)
+	form = PostForm(request.POST or None,instance=instance)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		messages.success(request,"Tu <a href='#'>post</a> se ha actualizado :D", extra_tags="html_safe")
+		return HttpResponseRedirect(instance.get_absolute_url())
+	context={
+	"titulo":"instance.titulo",
+	"instance":instance,
+	"form": form,
+	}
+	return render(request,"post_form.html",context)
 
-def post_delete(request):
-    return HttpResponse("<h1>>:V</h1>")
+def post_delete(request,id=None):
+    instance = get_object_or_404(Post, id= id)
+    instance.delete()
+    messages.success(request, "Tu post se ha borrado :(")
+    return redirect("posts:list")
